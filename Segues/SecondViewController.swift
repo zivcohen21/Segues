880ec8c55fc8000434ec511be4acdeede05e6e96
Ziv Cohen <zivcohen21@gmail.com>
2018-08-22 19:18:01 +0300

    init commit

---
+++ Segues/SecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.swift
+//  Segues
+//
+//  Created by matan elimelech on 21/08/2018.
+//  Copyright © 2018 Moveo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    var textPassedOver : String?
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        label.text = textPassedOver
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Segues/ViewController.swift
+++ Segues/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        performSegue(withIdentifier: "goToSecondScreen", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToSecondScreen" {
+            let destinationVC = segue.destination as! SecondViewController
+            destinationVC.textPassedOver = textField.text!
+        }
+    }
+    
 }
 
